fc18f9cf49a6db92ecb8a7aeeee4890492aedf12
jiangxiuli <jiangxiulilinux@163.com>
2018-06-04 16:22:24 +0800

    commit haha

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -9,7 +9,7 @@ package com.wakaleo.gameoflife.domain;
  */
 public enum Cell {
 	// Symbols to represent cell status
-    LIVE_CELL("P"), DEAD_CELL(".");
+    LIVE_CELL("c"), DEAD_CELL(".");
 
     private String symbol;
 
